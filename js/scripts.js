ff8824159a86621d585040d1df49e09622788600
Jordan Cole <jthcole@gmail.com>
2019-03-26 21:54:19 -0400

    added javascript animation and icons in nav

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,5 @@
-// Make an array of quotes.
+// Some animation to enhance your visit to my site
 
-var myQuotes = new Array();
-myQuotes[0] = "Life without adventure would be deadly&nbspdull.";
-myQuotes[1] = "Trust should be the basis for all our moral training.";
-myQuotes[2] = "There is no teaching to compare with example.";
 
-// Get a new random number between 0 and the length of the array.
-var myRandom = Math.floor(Math.random()*myQuotes.length);
-
-// Update the quote (put an id="myQuote" or whatever)
-$('#randomQuote').html(myQuote[myRandom]);
+$('.headerImg').hide();
+$('.headerImg').fadeIn(1000);
